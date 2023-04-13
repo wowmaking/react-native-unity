e095a34e70b1e6286ec3caba0462ccbca1c016fa
Dmitry Markevich <mdv.dop@gmail.com>
2023-04-13 16:49:08 +0300

    Modified iOS and added Android postprocessing

--- unity/Editor/RNBuild.cs
+++ unity/Editor/RNBuild.cs
@@ -1,5 +1,9 @@
+using System.IO;
+using System.Text.RegularExpressions;
 using UnityEditor;
+using UnityEditor.Android;
 using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
 
 namespace Wowmaking.RNU.Editor
 {
@@ -8,23 +12,50 @@ namespace Wowmaking.RNU.Editor
         [PostProcessBuild]
         public static void OnPostProcessBuild(BuildTarget target, string pathToBuiltProject)
         {
+            /* iOS project modification */
             if (target == BuildTarget.iOS)
             {
-                var projectPath = UnityEditor.iOS.Xcode.PBXProject.GetPBXProjectPath(pathToBuiltProject);
-                var project = new UnityEditor.iOS.Xcode.PBXProject();
-
+                string projectPath = PBXProject.GetPBXProjectPath(pathToBuiltProject);
+                PBXProject project = new PBXProject();
                 project.ReadFromFile(projectPath);
-                var targetId = project.GetUnityFrameworkTargetGuid();
 
+                string mainTargetGuid = project.GetUnityMainTargetGuid();
+                string unityFrameworkTargetGuid = project.GetUnityFrameworkTargetGuid();
+                string dataFolderGuid = project.FindFileGuidByRealPath("Data");
+
+                /* Detach Data folder from Unity-iPhone target */
+                project.RemoveFileFromBuild(
+                    mainTargetGuid,
+                    dataFolderGuid
+                );
+
+                /* Attach Data folder to UnityFramework target */
                 project.AddFileToBuildSection(
-                    targetId,
-                    project.GetResourcesBuildPhaseByTarget(targetId),
-                    project.FindFileGuidByRealPath("Data")
+                    unityFrameworkTargetGuid,
+                    project.GetResourcesBuildPhaseByTarget(unityFrameworkTargetGuid),
+                    dataFolderGuid
                 );
 
                 project.WriteToFile(projectPath);
             }
+
+            /* Android project modification */
+            if (target == BuildTarget.Android)
+            {
+                /* Fix ndk path in build.gradle */
+                string gradleFile = Path.Combine(pathToBuiltProject, "unityLibrary/build.gradle");
+                string gradleText = File.ReadAllText(gradleFile);
+                gradleText = gradleText.Replace("android.ndkDirectory", "\"" + AndroidExternalToolsSettings.ndkRootPath + "\"");
+                File.WriteAllText(gradleFile, gradleText);
+
+                /* Delete intent from AndroidManifest.xml */
+                string manifestFile = Path.Combine(pathToBuiltProject, "unityLibrary/src/main/AndroidManifest.xml");
+                string manifestText = File.ReadAllText(manifestFile);
+                manifestText = Regex.Replace(manifestText, @"<application .*>", "<application>");
+                Regex regex = new Regex(@"<activity.*>(\s|\S)+?</activity>", RegexOptions.Multiline);
+                manifestText = regex.Replace(manifestText, "");
+                File.WriteAllText(manifestFile, manifestText);
+            }
         }
     }
-
 }
